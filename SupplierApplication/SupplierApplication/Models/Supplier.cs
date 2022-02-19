a02f7e73865702e0f64eea478d98fdeee17a5cc5
Letjobana <suzanm@gerbertech.co.za>
2022-02-19 21:10:21 +0200

    Added a model folder and a class called Supplier

---
+++ SupplierApplication/SupplierApplication/Models/Supplier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SupplierApplication.Models
+{
+    public class Supplier
+    {
+        [Key]
+        public string Id { get; set; }
+        [Display(Name = "Supplier Name")]
+        [Column(TypeName = "nvarchar(20)")]
+        public string Name { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string Email { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string Address { get; set; }
+    }
+}
