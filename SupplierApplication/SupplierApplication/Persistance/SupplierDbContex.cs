da882e218b3487bb01c74690627fac73aa62ff39
Letjobana <suzanm@gerbertech.co.za>
2022-02-19 21:20:19 +0200

    Added a persistance folder and a class called SupplierDbContext

---
+++ SupplierApplication/SupplierApplication/Persistance/SupplierDbContex.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupplierApplication.Models;
+
+namespace SupplierApplication.Persistance
+{
+    public class SupplierDbContex : DbContext
+    {
+        public SupplierDbContex(DbContextOptions<SupplierDbContex> options) : base(options)
+        {
+        }
+        public DbSet<Supplier> GetSuppliers { get; set; }
+    }
+}
